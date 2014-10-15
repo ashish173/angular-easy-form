926b658f23f7d53f4b876810bc1eab96b4bb6ec6
Everett Qin <everett.qyf@gmail.com>
2014-10-15 17:48:43 +0200

    add remote ui-select

--- .tmp/templates.js
+++ .tmp/templates.js
@@ -313,7 +313,9 @@ angular.module("easy-form/templates/inputs/ui-select-multiple.html", []).run(["$
     "           reset-search-input=\"\"\n" +
     "           multiple>\n" +
     "    <ui-select-match placeholder=\"placeholder\">{{$item.text}}</ui-select-match>\n" +
-    "    <ui-select-choices repeat=\"item in options.uiSelect.collection\">\n" +
+    "    <ui-select-choices repeat=\"item in options.uiSelect.collection\"\n" +
+    "                       refresh=\"options.uiSelect.refresh($select.search)\"\n" +
+    "                       refresh-delay=\"0\">\n" +
     "        <div ng-bind-html=\"item.text | highlight: $select.search\"></div>\n" +
     "    </ui-select-choices>\n" +
     "</ui-select>");
@@ -325,10 +327,13 @@ angular.module("easy-form/templates/inputs/ui-select.html", []).run(["$templateC
     "           ng-disabled=\"\"\n" +
     "           reset-search-input=\"\">\n" +
     "    <ui-select-match placeholder=\"placeholder\">{{$select.selected.text}}</ui-select-match>\n" +
-    "    <ui-select-choices repeat=\"item in options.uiSelect.collection\">\n" +
+    "    <ui-select-choices repeat=\"item in options.uiSelect.collection\"\n" +
+    "                       refresh=\"options.uiSelect.refresh($select.search)\"\n" +
+    "                       refresh-delay=\"0\">\n" +
     "        <div ng-bind-html=\"item.text | highlight: $select.search\"></div>\n" +
     "    </ui-select-choices>\n" +
-    "</ui-select>");
+    "</ui-select>\n" +
+    "");
 }]);
 
 angular.module("easy-form/templates/inputs/url.html", []).run(["$templateCache", function($templateCache) {
